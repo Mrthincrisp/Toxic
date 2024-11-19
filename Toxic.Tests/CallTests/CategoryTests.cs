ca5e577b30225fc0edfde2df29d52e37425a3c7c
Derek Swann <148038820+Mrthincrisp@users.noreply.github.com>
2024-11-18 21:48:51 -0600

    category unit tests

--- Toxic.Tests/CallTests/CategoryTests.cs
+++ Toxic.Tests/CallTests/CategoryTests.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Moq;
+using AutoMapper;
+using Toxic.Mapper;
+using Toxic.DTOs;
 using Toxic.Interfaces;
 using Toxic.Models;
 using Toxic.Services;
@@ -14,14 +17,22 @@ namespace Toxic.Tests.CallTests
     {
         private readonly Mock<ICategoryRepository> _mockCategoryRepository;
         private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
         public CategoryTests()
         {
             _mockCategoryRepository = new Mock<ICategoryRepository>();
+
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+            _mapper = mappingConfig.CreateMapper();
+
             _categoryService = new CategoryService(_mockCategoryRepository.Object);
         }
 
-        [Fact] 
+        [Fact] //get all categories
         public async Task GetCategoriesAsync_WhenCalled_ReturnCategoriesAsync()
         {
             var categories = new List<Category>
@@ -39,20 +50,81 @@ namespace Toxic.Tests.CallTests
             Assert.Equal(3, results.Count);
         }
 
-        [Fact]
-        public async Task SingleCategoryTest()
+        [Fact] //Get single category
+        public async Task SingleCategoryTest_WhenCalled_ReturnSingleCategory()
         {
             var category = new Category
             {
                 Id = 1
             };
 
-            _mockCategoryRepository.Setup(repo => repo.GetCategoryByIdAsync(category.Id));
+            _mockCategoryRepository.Setup(repo => repo.GetCategoryByIdAsync(category.Id)).ReturnsAsync(category);
 
             var results = await _categoryService.GetCategoryByIdAsync(category.Id);
 
             Assert.NotNull(results);
             Assert.Equal(category.Id, results.Id);
+        }
+
+        [Fact] // Delete category
+        public async Task DeleteACategory_WhenCalled_ReturnsDeletedCategoryAsync()
+        {
+            var category = new Category
+            {
+                Id = 1,
+                Title = "test"
+            };
+
+            _mockCategoryRepository.Setup(x => x.GetCategoryByIdAsync(category.Id)).ReturnsAsync(category);
+
+            await _categoryService.DeleteCategoryAsync(category.Id);
+
+            _mockCategoryRepository.Verify(x => x.DeleteCategoryAsync(category.Id), Times.Once);
+
+            _mockCategoryRepository.Setup(x => x.GetCategoryByIdAsync(category.Id)).ReturnsAsync((Category)null);
+        }
+
+        [Fact] //Create category
+        public async Task CreateCategoryAsync_WhenCalled_ReturnsnewCateGoryWhenCalled()
+        {
+
+            var categoryDTO = new UpsertCategoryDTO
+            {
+                Title = "Test",
+            };
+
+            var category = _mapper.Map<Category>(categoryDTO);
+
+            _mockCategoryRepository.Setup(x => x.CreateCategoryAsync(_mapper, categoryDTO)).ReturnsAsync(category);
+
+            var result = await _categoryService.CreateCategoryAsync(_mapper, categoryDTO);
+
+            Assert.NotNull(result);
+            Assert.Equal(categoryDTO.Title, result.Title);
+
+
+        }
+
+        [Fact] //Update category
+        public async Task UpdateCategoryAsync_WhenCalled_ReturnUpdatedCategory()
+        {
+            int id = 1;
+
+            var categoryDTO = new UpsertCategoryDTO
+            {
+                Title = "update me"
+            };
+
+            var category = _mapper.Map<Category>(categoryDTO);
+
+            _mockCategoryRepository.Setup(x => x.GetCategoryByIdAsync(id)).ReturnsAsync(category);
+            _mockCategoryRepository.Setup(x => x.UpdateCategoryAsync(id, _mapper, categoryDTO)).ReturnsAsync(category);
+
+            var result = await _categoryService.UpdateCategoryAsync(id, _mapper, categoryDTO);
+
+            Assert.NotNull(result);
+            Assert.Equal(categoryDTO.Title, result.Title);
+
         }
     }
 }
